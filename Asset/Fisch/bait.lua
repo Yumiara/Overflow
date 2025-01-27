25520343383b4f02843c338f0deebf35bd35ee8d
Yumiria <154666142+Yumiara@users.noreply.github.com>
2025-01-27 11:44:31 +0700

    Create bait.lua

---
+++ Asset/Fisch/bait.lua
@@ -0,0 +1,2 @@
+GG = (getgenv and getgenv()) or _G or shared;
+GG.BaitList = {"Magnet","Worm","Peppermint Worm","Coal","Squid","Shrimp","Give","Kraken Tentacle","Insect","Deep Coral","Night Shrimp","Hangman's Hook","Super Flakes","Seaweed","Instant Catcher","Shark Head","Fish Head","Weird Algae","Garbage","Holly Berry","Rapid Catcher","Minnow","Maggot","Coral","Flakes","Aurora Bait","Bagel","Truffle Worm"}
