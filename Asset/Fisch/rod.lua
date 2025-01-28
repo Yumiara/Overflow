5f4800a397fa1d585166f74f8c4bbd37db504f10
Yumiria <154666142+Yumiara@users.noreply.github.com>
2025-01-28 18:49:43 +0700

    Update rod.lua

--- Asset/Fisch/rod.lua
+++ Asset/Fisch/rod.lua
@@ -2,3 +2,334 @@ GG.RodList = {"Long Rod","Developers Rod","Krampus's Rod","Mystic Staff","Fortun
 GG.BaitList = {"Magnet","Worm","Peppermint Worm","Coal","Squid","Shrimp","Give","Kraken Tentacle","Insect","Deep Coral","Night Shrimp","Hangman's Hook","Super Flakes","Seaweed","Instant Catcher","Shark Head","Fish Head","Weird Algae","Garbage","Holly Berry","Rapid Catcher","Minnow","Maggot","Coral","Flakes","Aurora Bait","Bagel","Truffle Worm"}
 GG.BobbersList = {"Dark Art Skull","White Festive Light","Snowman","Income","Rune Meg Bobber","Lava","Star","LED","Ancient Book","Gold Koi","Candy Corn","Crystallized Ball","Red Festive Light","Meteor Stone","Eye","Quiche","Evil Totem","Confetti Globe","Lightning","Anchor","Translucent","Santa Hat","Ancient Serpent Skull","Diamond","Sunken Ship","Mermaid","Kraken","Fire Snake","Seahorse","Huge","Mushroom","Infested","Gem","Skull","Compass","Evil Ducky","Firefin","Firework Rocket","Gold Coin","Gift Box","XMAS Ornament","Green Festive Light","Abyssal Skull","Abyssal Trident","Skeleton","Frozen Crown","Fake Pufferfish","Mythic Sea Star","Elf Hat","Carbon","Jingle Bell","Whaley","Firework","Evil Santa","Candy Cane","Reindeer Antlers","Frozen Serpent","Coral Reef","Mini Terrapin","Key of the Depths","Snowflake Crystal","Ancient Relic Bobber","North Star Bobber","Party Popper","Festive Party Hat","Elite Crystal","Amber","Mila","Vertigo Rock","Frozen","Jade Serpent Bobber","Fossil Fish","Founders Rainbow","Snow Globe","Pirate Wheel","Crown Of the Depths","Coal","Ocean King","Nautilis Shell","Pirate Hat","Cat","Bat","Drifter's Tale","Dreadspire","Leaf","Hour Glass","Clover","Shaped","Stock","Jack-O-Bobber","Wireframe","Brine","Lunar Serpent","Cube","Rubber Ducky","Mermaid's Pearl","Aurora","Heart","Coral Crown","Sun","Glimmer","Frozen Rubber Ducky"};
 GG.ItemList = {"Windset Totem","Tidebreaker","Blizzard Totem","Magic Thread","Tempest Totem","Ancient Thread","Heart Of Zeus","Flippers","Lunar Thread","Aurora Totem","Basic Diving Gear","Beginner Oxygen Tank","Witches Ingredient","Firework","Water Bubble","Avalanche Totem","Conception Conch","Glider","Zeus Storm Totem","Advanced Oxygen Tank","Winter Cloak","Super Flippers","Advanced Diving Gear","Crab Cage","Skin Crate","Bag of Presents","Elite Token","Regular Token","Treasure Map","Poseidon Wrath Totem","TNT","Handwritten Note","Fillionaire","Nuke","GPS","Meteor Totem","Pickaxe","Sundial Totem","Intermediate Oxygen Tank","Advanced Glider","Fish Radar","Basic Oxygen Tank","Eclipse Totem","Smokescreen Totem"};
+GG.Locations = {
+	All = {
+		Name = "Every";
+		Banner = "rbxassetid://18310847126";
+		Color = Color3.fromRGB(255, 255, 255);
+		BadgeId = 635398639427099;
+		Bobber = "Aurora";
+		Rewards = {
+			Coins = 10000;
+			Xp = 10000;
+		};
+	};
+	Moosewood = {
+		Name = "Moosewood Island";
+		Banner = "rbxassetid://18310954523";
+		Color = Color3.fromRGB(255, 94, 94);
+		BadgeId = 3871316346306270;
+		Bobber = "Rubber Ducky";
+		Rewards = {
+			Coins = 2000;
+			Xp = 2000;
+		};
+	};
+	["Keepers Altar"] = {
+		Name = "Keepers Altar";
+		Banner = "rbxassetid://131504032544120";
+		Color = Color3.fromRGB(63, 71, 185);
+		BadgeId = 772547227400435;
+		Bobber = "Skeleton";
+		Rewards = {
+			Coins = 1750;
+			Xp = 1750;
+		};
+	};
+	Roslit = {
+		Name = "Roslit Bay";
+		Banner = "rbxassetid://18542406497";
+		Color = Color3.fromRGB(255, 160, 64);
+		BadgeId = 225841574756551;
+		Bobber = "Fake Pufferfish";
+		Rewards = {
+			Coins = 2000;
+			Xp = 2000;
+		};
+	};
+	["Roslit Volcano"] = {
+		Name = "Roslit Volcano";
+		Banner = "rbxassetid://108037427819021";
+		Color = Color3.fromRGB(255, 160, 64);
+		BadgeId = 4479912445209985;
+		Bobber = "Lava";
+		Rewards = {
+			Coins = 2000;
+			Xp = 2000;
+		};
+	};
+	Terrapin = {
+		Name = "Terrapin Island";
+		Banner = "rbxassetid://18791315983";
+		Color = Color3.fromRGB(87, 255, 143);
+		BadgeId = 2994442057572213;
+		Bobber = "Mini Terrapin";
+		Rewards = {
+			Coins = 2000;
+			Xp = 2000;
+		};
+	};
+	Mushgrove = {
+		Name = "Mushgrove Swamp";
+		Banner = "rbxassetid://18952253458";
+		Color = Color3.fromRGB(150, 255, 102);
+		BadgeId = 4343979385420841;
+		Bobber = "Mushroom";
+		Rewards = {
+			Coins = 2000;
+			Xp = 2000;
+		};
+	};
+	Sunstone = {
+		Name = "Sunstone Island";
+		Banner = "rbxassetid://108032910283682";
+		Color = Color3.fromRGB(255, 236, 129);
+		BadgeId = 2903347774735091;
+		Bobber = "Sun";
+		Rewards = {
+			Coins = 2000;
+			Xp = 2000;
+		};
+	};
+	Snowcap = {
+		Name = "Snowcap Island";
+		Banner = "rbxassetid://101454347375454";
+		Color = Color3.fromRGB(165, 255, 214);
+		BadgeId = 4101868524305507;
+		Bobber = "Frozen";
+		Rewards = {
+			Coins = 2000;
+			Xp = 2000;
+		};
+	};
+	Ocean = {
+		Name = "Ocean";
+		Banner = "rbxassetid://18310945663";
+		Color = Color3.fromRGB(161, 199, 255);
+		BadgeId = 1045860405568363;
+		Bobber = "Anchor";
+		Rewards = {
+			Coins = 2000;
+			Xp = 2000;
+		};
+	};
+	Vertigo = {
+		Name = "Vertigo";
+		Banner = "rbxassetid://18952306740";
+		Color = Color3.fromRGB(127, 110, 255);
+		BadgeId = 3200076862852988;
+		Bobber = "Vertigo Rock";
+		Rewards = {
+			Coins = 2000;
+			Xp = 2000;
+		};
+	};
+	["Desolate Deep"] = {
+		Name = "Desolate Deep";
+		Banner = "rbxassetid://81573600788931";
+		Color = Color3.fromRGB(151, 187, 255);
+		BadgeId = 3161443122784523;
+		Bobber = "Infested";
+		Rewards = {
+			Coins = 2000;
+			Xp = 2000;
+		};
+	};
+	["Brine Pool"] = {
+		Name = "Brine Pool";
+		Banner = "rbxassetid://86510876700490";
+		Color = Color3.fromRGB(140, 255, 140);
+		BadgeId = 1535329670769985;
+		Bobber = "Brine";
+		Rewards = {
+			Coins = 2000;
+			Xp = 2000;
+		};
+	};
+	["Forsaken Shores"] = {
+		Name = "Forsaken Shores";
+		Banner = "rbxassetid://124958348694122";
+		Color = Color3.fromRGB(97, 73, 67);
+		BadgeId = 3591399443042848;
+		Bobber = "Pirate Wheel";
+		Rewards = {
+			Coins = 2000;
+			Xp = 2000;
+		};
+	};
+	["The Depths"] = {
+		Name = "The Depths";
+		Banner = "rbxassetid://115528091268474";
+		Color = Color3.fromRGB(97, 37, 37);
+		BadgeId = 2042505003241970;
+		Bobber = "Nautilis Shell";
+		Rewards = {
+			Coins = 2000;
+			Xp = 2000;
+		};
+	};
+	["Ancient Isle"] = {
+		Name = "Ancient Isle";
+		Banner = "rbxassetid://79846684203518";
+		Color = Color3.fromRGB(140, 255, 194);
+		BadgeId = 2054807830474099;
+		Bobber = "Amber";
+		Rewards = {
+			Coins = 50000;
+			Xp = 10000;
+		};
+	};
+	["Ancient Archives"] = {
+		Name = "Ancient Archives";
+		Banner = "rbxassetid://89908035795264";
+		Color = Color3.fromRGB(56, 84, 41);
+		BadgeId = 1666493813690822;
+		Bobber = "Ancient Book";
+		Rewards = {
+			Coins = 5000;
+			Xp = 2500;
+		};
+	};
+	["Overgrowth Caves"] = {
+		Name = "Overgrowth Caves";
+		Banner = "rbxassetid://105298630049350";
+		Color = Color3.fromRGB(1, 161, 185);
+		BadgeId = 272078520028439;
+		Bobber = "Frozen Rubber Ducky";
+		Rewards = {
+			Coins = 5000;
+			Xp = 2500;
+		};
+	};
+	["Frigid Cavern"] = {
+		Name = "Frigid Cavern";
+		Banner = "rbxassetid://89444673055559";
+		Color = Color3.fromRGB(1, 161, 185);
+		BadgeId = 843774301770016;
+		Bobber = "Frozen Rubber Ducky";
+		Rewards = {
+			Coins = 5000;
+			Xp = 2500;
+		};
+	};
+	["Cryogenic Canal"] = {
+		Name = "Cryogenic Canal";
+		Banner = "rbxassetid://117435163084140";
+		Color = Color3.fromRGB(1, 161, 185);
+		BadgeId = 4189906956026791;
+		Bobber = "Frozen Rubber Ducky";
+		Rewards = {
+			Coins = 5000;
+			Xp = 2500;
+		};
+	};
+	["Glacial Grotto"] = {
+		Name = "Glacial Grotto";
+		Banner = "rbxassetid://117435163084140";
+		Color = Color3.fromRGB(1, 161, 185);
+		BadgeId = 1396071619017862;
+		Bobber = "Frozen Rubber Ducky";
+		Rewards = {
+			Coins = 5000;
+			Xp = 2500;
+		};
+	};
+	["Grand Reef"] = {
+		Name = "Grand Reef";
+		Banner = "rbxassetid://115456199795193";
+		Color = Color3.fromRGB(59, 185, 116);
+		BadgeId = 968483808853747;
+		Bobber = "Coral Reef";
+		Rewards = {
+			Coins = 5000;
+			Xp = 2500;
+		};
+	};
+	["Atlantean Storm"] = {
+		Name = "Atlantean Storm";
+		Banner = "rbxassetid://102684908625295";
+		Color = Color3.fromRGB(52, 106, 191);
+		BadgeId = 32642745294476;
+		Rewards = {
+			Coins = 5000;
+			Xp = 2500;
+		};
+	};
+	Atlantis = {
+		Name = "Atlantis";
+		Banner = "rbxassetid://115456199795193";
+		Color = Color3.fromRGB(0, 200, 255);
+		BadgeId = 2070428894950149;
+		Bobber = "Coral Reef";
+		Rewards = {
+			Coins = 5000;
+			Xp = 2500;
+		};
+	};
+	["Archeological Site"] = {
+		Name = "Archeological Site";
+		Banner = "rbxassetid://71112448286941";
+		Color = Color3.fromRGB(231, 241, 32);
+		Hide = true;
+		Limited = true;
+		Rewards = {
+			Coins = 0;
+			Xp = 0;
+		};
+	};
+	FischFright = {
+		Name = "FischFright";
+		Banner = "rbxassetid://73235985228051";
+		Color = Color3.fromRGB(85, 0, 255);
+		Hide = true;
+		Limited = true;
+		Rewards = {
+			Coins = 0;
+			Xp = 0;
+		};
+	};
+	Fischgiving = {
+		Name = "Fischgiving";
+		Banner = "rbxassetid://133466398979921";
+		Color = Color3.fromRGB(220, 94, 21);
+		Hide = true;
+		Limited = true;
+		Rewards = {
+			Coins = 0;
+			Xp = 0;
+		};
+	};
+	Fischmas = {
+		Name = "Fischmas";
+		Banner = "rbxassetid://137233586307916";
+		Color = Color3.fromRGB(255, 88, 94);
+		Hide = true;
+		Limited = true;
+		Rewards = {
+			Coins = 0;
+			Xp = 0;
+		};
+	};
+	["Winter's Edge"] = {
+		Name = "Winter's Edge";
+		Banner = "rbxassetid://102684908625295";
+		Color = Color3.fromRGB(255, 88, 94);
+		Hide = true;
+		Limited = true;
+		Rewards = {
+			Coins = 0;
+			Xp = 0;
+		};
+	};
+	["Golden Tide"] = {
+		Name = "Golden Tide";
+		Banner = "rbxassetid://102684908625295";
+		Color = Color3.fromRGB(255, 88, 94);
+		Hide = true;
+		Limited = true;
+		Rewards = {
+			Coins = 0;
+			Xp = 0;
+		};
+	};
+};
